98ff1fc4936dd37c0bad4572b38b906ce2537d24
allen <allenarcenal@gmail.com>
2022-05-22 17:28:54 +0800

    add initial data

--- src/main/java/com/tamaraw/payrollbackend/PayrollBackendApplication.java
+++ src/main/java/com/tamaraw/payrollbackend/PayrollBackendApplication.java
@@ -1,13 +1,24 @@
 package com.tamaraw.payrollbackend;
 
+import com.tamaraw.payrollbackend.models.WebUser;
+import com.tamaraw.payrollbackend.repositories.WebUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PayrollBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(WebUserRepository webUserRepository) {
+		return args -> {
+			webUserRepository.save(new WebUser("admin", "welcome123"));
+		};
 	}
 
 }
--- src/main/java/com/tamaraw/payrollbackend/models/WebUser.java
+++ src/main/java/com/tamaraw/payrollbackend/models/WebUser.java
@@ -11,6 +11,11 @@ public class WebUser extends AuditableEntity{
 
     private String password;
 
+    public WebUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
